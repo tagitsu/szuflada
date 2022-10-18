b6a25122f80985fe89b77ef9fce3acf2eed3f1b9
Julia Diaków <tagitsu@gmail.com>
2022-10-18 19:37:05 +0200

    Add functionality - display form to adding new yarn

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,48 @@
 {
   'use strict';
 
-  const tplYarn = Handlebars.compile(document.querySelector('#yarn-box').innerHTML);
+  const select = {
+    container: {
+      yarnDrawer: '.yarn-drawer',
+      FormBox: '.square.form',
+    },
+    templateOf: {
+      yarnBox: '#yarn-box', 
+    },
+    buttons: {
+      addBtn: '.add-yarn',
+    },
+    elements: {
+      addYarnForm: '.form__add-yarn',
+    }
+  };
+  const classNames = {
+    visibleElement: 'active',
+    hiddenElement: 'hide',
+  };
+
+  const tplYarn = Handlebars.compile(document.querySelector(select.templateOf.yarnBox).innerHTML);
+  
+  const drawer = document.querySelector(select.container.yarnDrawer);
+  const addBtn = document.querySelector(select.buttons.addBtn);
+  const formBox = document.querySelector(select.container.FormBox);
+  const addYarnForm = document.querySelector(select.elements.addYarnForm);
+  console.log('formularz dodawania włóczki', addYarnForm);
+
+
+  // dodawnie włóczki
+  // obsługa przycisku dodającego włóczkę
+  addBtn.addEventListener('click', displayForm);
+
+  function displayForm() {
+    formBox.appendChild(addYarnForm);
+    addYarnForm.classList.remove(classNames.hiddenElement);
+    addBtn.classList.remove(classNames.visibleElement);
+    addBtn.classList.add(classNames.hiddenElement);
+  }
+
+  // obsługa formularza
+  
 
   const yarn = {
     brand: 'drops',
@@ -10,7 +51,7 @@
 
   let newYarn = tplYarn(yarn);
 
-  const square = document.querySelector('.yarn-drawer').insertAdjacentHTML('beforeend', newYarn);
+  const square = drawer.insertAdjacentHTML('beforeend', newYarn);
 
   
 }
